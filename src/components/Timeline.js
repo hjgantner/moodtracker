7e8916364b18403a79a3ead4e8533e5a8bd5037f
Henry <hankgantner@gmail.com>
2021-05-12 10:53:55 -0500

    completed the basic date sorting functionality.
    Timeline component pulls unique dates, maps over them, filtering the user's history and passes that to TimelineDay,
    TimelineDay utilizes flex columns and rows from bootstrap to map each item of the filtered data to the TimelineItem component, which
    actually displays the div with color and overlaytrigger
    
    Next steps: modify/improve styling, add animations, add events to TimelineItems to get more details about them on click

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,56 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TimelineItem from './TimelineItem';
-import { 
-    motion
-} from 'framer-motion';
+import TimelineDay from './TimelineDay';
+
+
 
 export default function Timeline ({history, userName}) {
-    console.log("moods in Timeline: ", history);
 
-    const dateSet = new Set();
 
-    function createUniqueDates(dates) {
-        // for(let i = 0; i < dates.length; i++) {
-        //     if()
-        // }
-    }
-    const variants = { 
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
-    }
+    const [dateSet, setDateSet] = useState(new Set());
 
-    const transition = {
-        type: "spring",
-        duration: 2.3,
-        stiffness: 30,
+    useEffect(() => {
+        setUniqueDates(history)
+        return () => {
+            
+        }
+    }, [history])
+
+    function setUniqueDates(moods) {
+       
+        console.log(moods);
+        var dates = moods.map(elem => {
+            return elem.insertedAt.split('T')[0];
+        });        
+        setDateSet(new Set(dates));
+        console.log([...dateSet]);
     }
 
     return (
-        
         <div className="container text-center">
-            <div className="container"></div>
-            <motion.div 
-                    initial="hidden"
-                    animate="visible"
-                    variants={variants}
-                    transition={transition}
-                >
-            {
-                history.filter( element => {
+           
 
-                })
+            {
+                [...dateSet].map( date => (
+                    
+                        <TimelineDay
+                        day={date}
+                        data= {
+                            history.filter( mood => {
+                                let elementDate = mood.insertedAt.split('T')[0];
+                                return elementDate === date;
+                            })
+                        }
+                    />
+                ))
+               
+                       
             }
-            <div className="d-inline-flex row">
+           
             
-                {
+                {/* {
                     history.map( element => 
                         <TimelineItem 
                             data={element}
                         />
                     )
-                }
-            </div>
-            </motion.div>
+                } */}
+            
         </div>
     );
 }
--- src/components/TimelineDay.js
+++ src/components/TimelineDay.js
@@ -1,11 +1,47 @@
-const TimelineDay = ({data}) => (
+import React from 'react';
+import TimelineItem from './TimelineItem';
+import { 
+    motion
+} from 'framer-motion';
 
-    data.map( element => 
-        <TimelineItem 
-            data={element}
-        />
-    )
+const TimelineDay = ({day, data}) => {
 
-);
+    // const variants = { 
+    //     hidden: { opacity: 0 },
+    //     visible: { opacity: 1 },
+    // }
+
+    // const transition = {
+    //     type: "spring",
+    //     duration: 2.3,
+    //     stiffness: 30,
+    // }
+    return (
+        
+        // <motion.div 
+        //     initial="hidden"
+        //     animate="visible"
+        //     variants={variants}
+        //     transition={transition}
+        // >
+        <div className="d-flex flex-column">
+            <div className="d-flex row">
+                <h4>{day}</h4>
+            </div>
+            <div className="d-flex row">
+                {
+                    data.map( element => 
+                        <TimelineItem 
+                            data={element}
+                        />
+                    )
+                }
+            </div>
+        </div>
+
+            
+        // </motion.div>
+    );
+};
 
 export default TimelineDay;
